2824982b13eb0edb3b1c11c35364ee43fbda1c66
kmeij <62712568+kmeij@users.noreply.github.com>
2020-04-10 17:51:42 +0100

    Update Ch2_Example3.py

--- LTPIP/Chapter2/Ch2_Example3.py
+++ LTPIP/Chapter2/Ch2_Example3.py
@@ -2,15 +2,15 @@
 # Chapter 2, Example 3 - Variables:
 
 # Create the following code in Python and execute in the Main window:
-# A) Create 3 variables called main, juice, banana 
-# B) Assign the following values to the first 3: 3.00, 0.75 and 1.00  
-# C) Write code to add all 3 together in a variable called ‘total’
-# D) Print it using the title “Total for lunch:  £“
+# A) Create 3 variables called main_course, juice, dessert 
+# B) Assign the following values to the variables: 3.00, 0.75 and 1.00 Â 
+# C) Write code to add all 3 together in a variable called â€˜totalâ€™
+# D) Output it using the 'title' input at the start (eg. â€œTotal for lunch: Â Â£â€œ).  The output must contain a Â£ symbol.
 
 title = input("Type an appropriate title for the calculations below: ") 
 _____ = 3.00
 _____ = 0.75
 _____ = 1.00
 _____ = _____ + _____ + _____
-
 print("_______________",_____)
+
