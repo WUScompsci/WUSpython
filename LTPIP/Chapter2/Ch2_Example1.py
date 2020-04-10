e16b91482557c9557d024f9ab9a7e549302e014d
kmeij <62712568+kmeij@users.noreply.github.com>
2020-04-10 16:00:05 +0100

    Add files via upload

---
+++ LTPIP/Chapter2/Ch2_Example1.py
@@ -0,0 +1,18 @@
+# -*- coding: cp1252 -*-
+#Chapter 2 - String methods:
+# Replace the _______ bits with the correct code.  The code below also has syntax errors.  Please sort it out and then run it in the Main window.
+
+astring = "You've done a good job"
+print("Original string:", astring)
+
+print("nUppercase string:", astring.________())
+print("To replace the string 'good' with a new string:",astring.__________("good","brilliant"))
+print("The new string is:",astring)
+
+print("The first occurence of 'v' is at index :",astring.__________('v'))
+print("The word 'done' starts at index",astring.____________("done"))
+
+input ("\nPress Enter to exit")
+
+
+                                      
